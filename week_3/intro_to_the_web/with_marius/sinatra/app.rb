d1c71473f9f883365593ebd278ce102bfe167ee3
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-30 13:09:24 +0100

    Added intro_to_the_web exercises with pair partners

---
+++ week_3/intro_to_the_web/with_marius/sinatra/app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+get '/' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/secret' do
+  "This is a secret"
+end
+
+get '/random-cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'
+  </div>"
+end
+
+post '/named-cat' do
+  p params[:name]
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
+end
