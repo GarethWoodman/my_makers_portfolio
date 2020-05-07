91cbab684207d5254db48529a19f08edf4f8c777
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-07 12:19:32 +0100

    User adds entries and is displayed on page

--- week_4/daily_diary/app.rb
+++ week_4/daily_diary/app.rb
@@ -1,4 +1,21 @@
 require 'sinatra/base'
+require './lib/diary'
 
 class DailyDiary < Sinatra::Base
+  get '/' do
+  end
+
+  get '/entries/new' do
+    erb :add_entries
+  end
+
+  post '/submit_entry' do
+    Diary.add(title: params[:title], entry: params[:entry])
+    redirect '/entries'
+  end
+
+  get '/entries' do
+    @diary = Diary.entries
+    erb :entries
+  end
 end
---
+++ week_4/daily_diary/spec/features/add_diary_entries_spec.rb
@@ -0,0 +1,14 @@
+feature "add entries" do
+
+  feature "adding a new diary entry" do
+    scenario "A user can add an entry to their Daily Diary" do
+      visit('/entries/new')
+      fill_in('entry', with: 'The day it all started')
+      fill_in('title', with: 'First entry')
+      click_button('Submit')
+
+      #link = 'http:localhost:9292/entries/1'
+      expect(page).to have_content 'First entry'
+    end
+  end
+end
