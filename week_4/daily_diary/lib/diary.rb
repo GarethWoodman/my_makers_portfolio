9d0b5c5c36e990131aba360f8cc6139891d2da7a
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-07 12:16:20 +0100

    Refactored Diary to connect to database

--- week_4/daily_diary/lib/diary.rb
+++ week_4/daily_diary/lib/diary.rb
@@ -1,19 +1,43 @@
+require 'pg'
+
 class Diary
-  attr_reader :entries
+  # attr_reader :entries
+  #
+  # def initialize
+  #   @entries = Array.new
+  # end
 
-  def initialize
-    @entries = Array.new
+  # def add(entry:, title:)
+  #   @entries << { :entry => entry, :title => title }
+  # end
+
+  def self.add(title:, entry:)
+    connection.exec("INSERT INTO diary (title, entry) VALUES('#{title}', '#{entry}')")
   end
 
-  def add(entry:, title:)
-    @entries << { :entry => entry, :title => title }
+  # def list_titles
+  #   entries.map { |entry| entry[:title] }
+  # end
+
+  def self.list_titles
+    entries.map { |entry| entry['title'] }
   end
 
-  def list_titles
-    entries.map { |entry| entry[:title] }
+  # def get_entry_by(title:)
+  #   entries.each { |entry| return entry[:entry] if entry[:title] == title }
+  # end
+
+  def self.get_entry_by(title:)
+    entries.each { |entry| return entry['entry'] if entry['title'] == title }
   end
 
-  def get_entry_by(title:)
-    entries.each { |entry| return entry[:entry] if entry[:title] == title }
+  def self.entries
+    connection.exec("SELECT * FROM diary")
+  end
+
+  private
+  def self.connection
+    ENV['ENVIRONMENT'] == 'test' ? db = 'daily_diary_test' : db = 'daily_diary'
+    PG.connect(dbname: db)
   end
 end
--- week_4/daily_diary/spec/diary_spec.rb
+++ week_4/daily_diary/spec/diary_spec.rb
@@ -6,31 +6,31 @@ describe Diary do
   let(:titles)   { ["First entry", "Second entry", "Third entry"] }
 
   before :each do
-    subject.add(entry: "Today is ok",   title: titles[0])
-    subject.add(entry: "Today is meh",  title: titles[1])
-    subject.add(entry: "Today is bad",  title: titles[2])
+    Diary.add(title: titles[0], entry: "Today is ok")
+    Diary.add(title: titles[1], entry: "Today is meh")
+    Diary.add(title: titles[2], entry: "Today is bad")
   end
   # ----------------------------------------------------------------
 
-  describe '#add(entry:)' do
+  describe '#add(title:, entry:)' do
     it 'returns entry' do
-      expect(subject.entries.first[:entry]).to eq "Today is ok"
+      expect(Diary.entries.first['entry']).to eq "Today is ok"
     end
 
     it 'returns title' do
-      expect(subject.entries.first[:title]).to eq "First entry"
+      expect(Diary.entries.first['title']).to eq "First entry"
     end
   end
 
   describe '#list_titles' do
     it 'returns all entry titles' do
-      expect(subject.list_titles).to eq titles
+      expect(Diary.list_titles).to eq titles
     end
   end
 
   describe '#get_entry_by(title:)' do
     it 'returns entry by title' do
-      expect(subject.get_entry_by(title: titles[0])).to eq "Today is ok"
+      expect(Diary.get_entry_by(title: titles[0])).to eq "Today is ok"
     end
   end
 end
