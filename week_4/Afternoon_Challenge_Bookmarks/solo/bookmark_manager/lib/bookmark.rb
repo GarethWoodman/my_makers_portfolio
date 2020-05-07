255039ee65f31e8e921acf465a342c977fbfd764
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-07 14:34:57 +0100

    Cleaned up bookmark

--- week_4/Afternoon_Challenge_Bookmarks/solo/bookmark_manager/lib/bookmark.rb
+++ week_4/Afternoon_Challenge_Bookmarks/solo/bookmark_manager/lib/bookmark.rb
@@ -11,17 +11,25 @@ class Bookmark
 
   def self.all
     bookmarks.map do |bookmark|
-      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+      Bookmark.new(id: bookmark['id'],
+                   title: bookmark['title'],
+                   url: bookmark['url'])
     end
   end
 
   def self.create(url:, title:)
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
-    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+    result = connection.exec("INSERT INTO bookmarks (url, title)
+                              VALUES('#{url}', '#{title}')
+                              RETURNING id, title, url;")
+
+    Bookmark.new(id: result[0]['id'],
+                 title: result[0]['title'],
+                 url: result[0]['url'])
   end
 
   def self.connection
-    ENV['ENVIRONMENT'] == 'test' ? db = 'bookmark_manager_test' : db = 'bookmark_manager'
+    ENV['ENVIRONMENT'] == 'test' ? db = 'bookmark_manager_test' :
+                                   db = 'bookmark_manager'
     PG.connect :dbname => db
   end
 
