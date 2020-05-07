6fcd382b808d08eec1f96ada95eee8a771b9bb26
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-07 12:18:39 +0100

    Cleaned up diary

--- week_4/daily_diary/lib/diary.rb
+++ week_4/daily_diary/lib/diary.rb
@@ -1,31 +1,13 @@
 require 'pg'
 
 class Diary
-  # attr_reader :entries
-  #
-  # def initialize
-  #   @entries = Array.new
-  # end
-
-  # def add(entry:, title:)
-  #   @entries << { :entry => entry, :title => title }
-  # end
-
   def self.add(title:, entry:)
     connection.exec("INSERT INTO diary (title, entry) VALUES('#{title}', '#{entry}')")
   end
 
-  # def list_titles
-  #   entries.map { |entry| entry[:title] }
-  # end
-
   def self.list_titles
     entries.map { |entry| entry['title'] }
   end
-
-  # def get_entry_by(title:)
-  #   entries.each { |entry| return entry[:entry] if entry[:title] == title }
-  # end
 
   def self.get_entry_by(title:)
     entries.each { |entry| return entry['entry'] if entry['title'] == title }
