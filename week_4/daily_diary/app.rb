9b65037e6bcbdcdf0a8bcd130ac009b1793e1b18
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-08 08:20:39 +0100

    Refactored app

--- week_4/daily_diary/app.rb
+++ week_4/daily_diary/app.rb
@@ -2,21 +2,18 @@ require 'sinatra/base'
 require './lib/diary'
 
 class DailyDiary < Sinatra::Base
-  get '/' do
+  get '/entries' do
+    @entries = Diary.entries
+    erb :index
   end
 
   get '/entries/new' do
-    erb :add_entries
+    erb :new
   end
 
-  post '/submit_entry' do
+  post '/entries' do
     Diary.add(title: params[:title], entry: params[:entry])
     redirect '/entries'
-  end
-
-  get '/entries' do
-    @diary = Diary.entries
-    erb :entries
   end
 
   get '/entries/:id' do
